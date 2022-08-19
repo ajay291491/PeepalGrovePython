4fe66bad0403becedff53a9b595d767cc571ef7e
ajay291491@gmail.com <ajay291491@gmail.com>
2022-08-19 11:37:25 +0100

    updating course session 1 material

--- HelloWrold.py
+++ HelloWrold.py
@@ -1,2 +1,42 @@
+"""
+Topic 1 : What is variable
+"""
+
+"""
+# Topic 2 : Understanding string Variable
+name = "Ajayaghosh V L"
+# print('Hello '  + name  +  ', How are you !!')  # Method : String concatenation
+# print('Hello {}, How are you !!'.format(name)) # Method : format
+# print (type(name))
+# print (len(name))
+# print(name[11:])
+# print(name[:11])
+# print(name.lower())
+"""
+
+"""
+# Topic 3 : Understanding Numeric Variable
+num_one = 10
+num_two = 20
+total = (num_one + num_two)
+print("Sum of", num_one, "and", num_two, "is :", total)
+# Excercise re-write above program using format
+print(type(total))
+"""
+
+"""
+# Topic 4 : Understanding float Variable
+num_one = 10.5
+num_two = 20.6
+total = (num_one + num_two)
+print("Sum of", num_one, "and", num_two, "is :", total)
+# Excercise re-write above program using format
+print(type(total))
+"""
+
+
+"""
+# Topic 5 : Accessing input externally
 name = input('Enter Your name : ')
 print('Hello {}'.format(name))
+"""
